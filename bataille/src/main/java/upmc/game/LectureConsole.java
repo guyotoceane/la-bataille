1d4cc3540c735d51a9a0d8482d6f6328cf78f382
oguyot <guyot.oceane1@gmail.com>
2017-10-14 16:21:57 +0100

    modification de la classe lecture console / fonctionnel

--- bataille/src/main/java/upmc/game/LectureConsole.java
+++ bataille/src/main/java/upmc/game/LectureConsole.java
@@ -6,15 +6,35 @@
 package upmc.game;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
- *
  * @author licence
  */
 public class LectureConsole implements LecturePseudo {
 
+    private String player1;
+    private String player2;
+
+    private ArrayList <String> pseudos;
+
     public ArrayList<String> lirePseudo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        add_pseudo();
+        return pseudos;
     }
-    
+
+    private void add_pseudo(){
+        Scanner console = new Scanner(System.in);
+        pseudos = new ArrayList<String>();
+
+        for(int i=1; i<3 ; i++){
+            System.out.print("Entrez le nom du joueur "+ i +" : ");
+            System.out.flush();
+            pseudos.add(console.nextLine());
+        }
+
+
+    }
+
+
 }
